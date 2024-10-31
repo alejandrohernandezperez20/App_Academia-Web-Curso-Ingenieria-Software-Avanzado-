2b381f8e09945798447ecd69404e0be722f1e8bf
alejandrohernandezperez20 <alexhp121@gmail.com>
2024-10-31 10:30:55 +0100

    Con base de datos ya preparado y hecho la base de datos academia y tablas, hecho registro de notas del alumno en registro_notas1

---
+++ bd.sql
@@ -0,0 +1,11 @@
+--CREA LA BASE DE DATOS---
+CREATE DATABASE academia;
+
+--empezando a usar  la base de datos academia
+USE academia;
+--Se crea la tabla nontas en la base de datos academia
+CREATE TABLE notas(
+    nombre VARCHAR(30),
+    identificacion INT(15),
+    calificacion VARCHAR(15)
+);
