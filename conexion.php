970898e2bfad4b3ea5c4e803f16fa126e2f256a1
alejandrohernandezperez20 <alexhp121@gmail.com>
2024-11-06 08:52:23 +0100

    prueba

--- conexion.php
+++ conexion.php
@@ -10,6 +10,7 @@
         content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!-- Bootstrap CSS v5.2.1 -->
+     <!-- Prueba-->
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         rel="stylesheet"
